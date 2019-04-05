4c8d09455fed0fbe8fdd76c67877551725e18bde
Bily General <billyjohn.general@gmail.com>
2019-04-04 17:19:01 -0700

    - Added Filter input and button

--- pkg/routes.py
+++ pkg/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from pkg import app
 from pkg.models import Computer
 
@@ -11,6 +11,8 @@ comps = Computer.query.all()
 def home():
     return render_template('home.html', comps = comps)
 
-@app.route("/about")
-def about():
-    return render_template('about.html')
+@app.route("/", methods=['POST'])
+def search():
+    search = request.form['search']
+    processed_search = search.upper()
+    return processed_search
